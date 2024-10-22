ad8ecc9e98024a861591afc66ac083c16129fd60
xianqGG <63194896+xianqGG@users.noreply.github.com>
2024-10-22 14:05:55 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["xiagg.tpddns.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "xiaoxiagg=";
+pub const PUBLIC_RS_PUB_KEY: &str = "gHpVcWmXyQaRfZlBnTsKgWdQiJhDkSoJeBvFtHpReLcZsJaOu=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
